de44e1ff0dfa343668a651a27251a98c8883bc4b
Aleksei-P-88 <aleksei.pshenko@gmail.com>
2020-09-18 23:27:24 +0300

    cycles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
-"use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-if (4) {
-    console.log("Ok!")
-} else {
-    console.log("Error")
-}
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+    for ( let i=0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+
+        if  ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            i--;
+            console.log('error');
+        }
+    
+    }
+
+    if (personalMovieDB.count < 10) {
+        console.log ('Просмотрено мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log ('Вы классический зритель');
+    }  else if (personalMovieDB.count >= 30) {
+        console.log ('Вы киноман');
+    } else {
+        console.log ('Произошла ошибка');
+    } 
+
+    console.log(personalMovieDB);
